e56402d4219110ae5bb7d5e8431e5a964c900285
tom <wmttom@gmail.com>
2015-10-16 17:25:43 +0800

    init

---
+++ loom.py
@@ -0,0 +1,21 @@
+import sublime, sublime_plugin
+
+from collections import OrderedDict
+
+
+class MklistCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        all_content_region = sublime.Region(0, self.view.size())
+        all_content = self.view.substr(all_content_region)
+        lines = ['"'+i+'",' for i in all_content.split("\n") if i and i.rstrip()]
+        result = "\n".join(lines)
+        self.view.replace(edit, all_content_region, result)
+
+
+class DistinctCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        all_content_region = sublime.Region(0, self.view.size())
+        all_content = self.view.substr(all_content_region)
+        lines = OrderedDict((i,None) for i in all_content.split("\n") if i and i.rstrip())
+        result = "\n".join(lines.keys())
+        self.view.replace(edit, all_content_region, result)
