51a3d1155271cacce6a772a1da1583e1da8673d0
tom <wmttom@gmail.com>
2016-03-31 16:15:30 +0800

    add camel_to_underline underline_to_camel

--- loom.py
+++ loom.py
@@ -25,3 +25,38 @@ class DistinctCommand(sublime_plugin.TextCommand):
         lines = OrderedDict((i,None) for i in all_content.split("\n") if i and i.rstrip())
         result = "\n".join(lines.keys())
         self.view.replace(edit, all_content_region, result)
+
+class Underline2camelCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        all_content_region = sublime.Region(0, self.view.size())
+        all_content = self.view.substr(all_content_region)
+        lines = OrderedDict((self.underline_to_camel(i),None) for i in all_content.split("\n") if i and i.strip())
+        result = "\n".join(lines.keys())
+        self.view.replace(edit, all_content_region, result)
+
+    @staticmethod
+    def underline_to_camel(input_str):
+        res = ""
+        for word in input_str.split("_"):
+            res += word.capitalize()
+        res = res.replace("Ios", "iOS")
+        return res
+
+
+class Camel2underlineCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        all_content_region = sublime.Region(0, self.view.size())
+        all_content = self.view.substr(all_content_region)
+        lines = OrderedDict((self.camel_to_underline(i),None) for i in all_content.split("\n") if i and i.strip())
+        result = "\n".join(lines.keys())
+        self.view.replace(edit, all_content_region, result)
+
+    @staticmethod
+    def camel_to_underline(input_str):
+        res = ""
+        input_str = input_str.replace("iOS", "Ios")
+        for index, letter in enumerate(input_str):
+            pre = "_" if index else ""
+            res += letter if not letter.isupper() else "{0}{1}".format(
+                pre, letter.lower())
+        return res
